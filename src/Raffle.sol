4abf12f91b42b7392be40e2bf9f0c700fe502610
GoudhamT <goudhamvel.88@gmail.com>
2025-12-20 11:08:29 -0800

    constructor added

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,7 +3,13 @@
 pragma solidity 0.8.19;
 
 contract Raffle {
-    function enterRaffle() public {}
+    uint256 private immutable i_enteranceFee;
+
+    constructor(uint256 _enteranceFee) {
+        i_enteranceFee = _enteranceFee;
+    }
+
+    function enterRaffle() public payable {}
 
     function pickWinner() public {}
 }
